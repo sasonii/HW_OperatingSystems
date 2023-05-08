3911fe186127d4c66dd761b1f8f8b0efb8ee720d
noapr <pariente@campus.technion.ac.il>
2023-05-08 21:52:17 +0300

    implements ctrlC

--- hw1/wet/skeleton_smash/signals.cpp
+++ hw1/wet/skeleton_smash/signals.cpp
@@ -10,7 +10,22 @@ void ctrlZHandler(int sig_num) {
 }
 
 void ctrlCHandler(int sig_num) {
-  // TODO: Add your implementation
+    std::cout << "smash: got ctrl-C" << std::endl;
+    SmallShell& smash = SmallShell::getInstance();
+
+    if (smash.current_foreground_job != nullptr) {
+        int job_id = smash.current_foreground_job->getJobId();
+        pid_t pid = smash.current_foreground_job->getJobProcessId();
+
+        if (kill(pid, SIGKILL) == -1) {
+            // TODO: do we need to add exceptions?
+            return;
+        }
+
+        smash.current_foreground_job = nullptr;
+        smash.jobsList.removeJobById(job_id);
+        std::cout << "smash: process " << pid << " was killed" << std::endl;
+    }
 }
 
 void alarmHandler(int sig_num) {

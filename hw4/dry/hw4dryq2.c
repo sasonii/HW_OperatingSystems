93e8b03b0142bf3e702f49a37a3fd2347dd1c572
DESKTOP-9KJIAH2\Dell <sasonshmuellameey@gmail.com>
2023-07-06 18:34:41 +0300

    updated dry

--- hw4/dry/hw4dryq2.c
+++ hw4/dry/hw4dryq2.c
@@ -1,10 +1,18 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-    void *ptr = malloc(atoi(argv[1]));
-    if (!ptr){
-            return 1;
+int main(int argc, char* argv[]) {
+    int size = atoi(argv[1]);
+	
+getpid();
+    char* buffer = (char*)malloc(size);
+getpid();	
+    if (buffer == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
     }
-    free(ptr);
+
+    free(buffer);
+
     return 0;
-}
+}	
